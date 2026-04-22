f050912914000c6cb8f3265f06376a3dc2fa6923
Robin Engvall <robinengvall96@gmail.com>
2026-04-22 21:53:34 +0200

    removed shorts with css

---
+++ content/content.js
@@ -0,0 +1,11 @@
+
+//
+// function HideShorts() {
+//   document.querySelectorAll("ytd-rich-shelf-renderer").forEach(el => {
+//     el.style.display = "none";
+//
+//   });
+//
+// }
+//
+// HideShorts();
